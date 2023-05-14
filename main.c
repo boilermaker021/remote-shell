41e9f198d0d8088c04d205ee9ce58723976abc16
Nathan <nathan.leakeas@gmail.com>
2023-05-14 14:32:25 -0500

    replace pipes with pseudoterminals

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+#define _XOPEN_SOURCE 600
+#define _GNU_SOURCE
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,11 +8,20 @@
 #include <netinet/in.h>
 #include <fcntl.h>
 #include <pthread.h>
+#include <pty.h>
 
-#define PORT 9997
+#define PORT 9992
 #define QUEUE_LENGTH 5
 
 extern char **environ;
+
+struct term_thread_info {
+  int term_fd;
+  int client_socket;
+};
+
+void *write_handle_thread(void *);
+void *read_handle_thread(void *);
 
 
 int main(int argc, char **argv) {
@@ -41,25 +53,56 @@ int main(int argc, char **argv) {
     struct sockaddr_in client_addr = {0};
     int client_addr_len = sizeof(client_addr);
     int client_sock = accept(master_sock, (struct sockaddr *)&client_addr, &client_addr_len);
-    int inpipefd[2];
-    int outpipefd[2];
-    pipe(inpipefd);
-    pipe(outpipefd);
+    int term_fd = posix_openpt(O_RDWR);
+    grantpt(term_fd);
+    unlockpt(term_fd);
+    char *slavename = ptsname(term_fd);
+    int slave_fd = open(slavename, O_RDWR);
+    if (slave_fd < 0) {
+      perror("Error opening slave pfd: ");
+    }
     if (fork() == 0) {
-      dup2(inpipefd[0], 0);
-      dup2(client_sock, 1);
-      char *shell = getenv("SHELL");
+      close(term_fd);
+      dup2(slave_fd, 0);
+      dup2(slave_fd, 1);
+      dup2(slave_fd, 2);
+      char shell[] = "bash";
       char *argv[] = {shell, NULL};
       execvp(shell, argv);
     }
+    close(slave_fd);
     char buf = {0};
-    while (read(client_sock, &buf, 1) > 0) {
-      if (buf != '\r') {
-        write(inpipefd[1], &buf, 1);
-      }
-    }
+    pthread_t w_thread;
+    pthread_t r_thread;
+    struct term_thread_info thread_params = {.client_socket= client_sock, .term_fd = term_fd};
+    pthread_create(&w_thread, NULL, write_handle_thread, (void *)&thread_params);
+    pthread_create(&r_thread, NULL, read_handle_thread, (void *)&thread_params);
+    pthread_join(w_thread, NULL);
+    pthread_join(r_thread, NULL);
     char exitmsg[] = "exit\n";
-    write(inpipefd[1], exitmsg, sizeof(exitmsg));
+    write(term_fd, exitmsg, sizeof(exitmsg));
+    close(term_fd);
     close(client_sock);
+  }
+  close(master_sock);
+}
+
+void *read_handle_thread(void *param) {
+  struct term_thread_info *info = (struct term_thread_info *)param;
+  char buf = 0;
+  while (read(info->client_socket, &buf, 1) > 0) {
+    if (buf != '\r') {
+      write(info->term_fd, &buf, 1);
+    }
+  }
+}
+
+void *write_handle_thread(void *param) {
+  struct term_thread_info *info = (struct term_thread_info *)param;
+  char buf = 0;
+  while (read(info->term_fd, &buf, 1) > 0) {
+    if (buf != '\r') {
+      write(info->client_socket, &buf, 1);
+    }
   }
 }
